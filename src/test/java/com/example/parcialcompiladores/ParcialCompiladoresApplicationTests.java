2c2173b10cb11d7ce49aa6452727e1403f68d09c
Nats306 <nati30062006@gmail.com>
2025-03-05 14:59:30 -0500

    init commit

---
+++ src/test/java/com/example/parcialcompiladores/ParcialCompiladoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialcompiladores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialCompiladoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
