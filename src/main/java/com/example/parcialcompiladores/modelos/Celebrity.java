c4c79777442f9fafe46183b029fd1ab8af740405
Nats306 <nati30062006@gmail.com>
2025-03-05 22:16:30 -0500

    Tu mensaje descriptivo del commit

--- src/main/java/com/example/parcialcompiladores/modelos/Celebrity.java
+++ src/main/java/com/example/parcialcompiladores/modelos/Celebrity.java
@@ -1,4 +1,15 @@
 package com.example.parcialcompiladores.modelos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity()
 public class Celebrity {
+
 }
