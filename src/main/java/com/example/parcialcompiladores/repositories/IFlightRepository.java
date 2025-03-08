8611d458bfb9b207d4b299b835beee92d8bc5985
Miguefon <miganfonre@gmail.com>
2025-03-08 11:22:46 -0500

    Implementacion Servicio de Flight y ControladorFlight

--- src/main/java/com/example/parcialcompiladores/repositories/IFlightRepository.java
+++ src/main/java/com/example/parcialcompiladores/repositories/IFlightRepository.java
@@ -4,5 +4,9 @@ import com.example.parcialcompiladores.modelos.Celebrity;
 import com.example.parcialcompiladores.modelos.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IFlightRepository extends JpaRepository<Flight, String> {
+
+    List<Flight> findByPurpose(String purpose);
 }
